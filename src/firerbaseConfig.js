291cffa4d9dcfb3d839a818004347f7eb8760fdc
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-12-05 10:32:14 +0100

    Update firerbaseConfig.js

--- src/firerbaseConfig.js
+++ src/firerbaseConfig.js
@@ -3,15 +3,15 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const app = initializeApp({
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID,
-    measurementId: REACT_APP_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
     //REALTIME DATABSE(firebase/database) NOT FIRESTORE(firebase/firestore)
-    databaseURL: REACT_APP_DATABASE_URL
+    databaseURL: process.env.REACT_APP_DATABASE_URL
 })
 
 export const auth = getAuth(app)
