600dd3d90e64c6089c35b1a20da4fd178fa84a8a
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-12-05 10:29:12 +0100

    Update UserDashboard.tsx

--- src/component/devHub/views/template/user/dashboards/UserDashboard.tsx
+++ src/component/devHub/views/template/user/dashboards/UserDashboard.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import { Typography } from '@mui/material'
-import { ComponentPropsType } from '../../../../middleware/helpers/ControllerTmplateView'
+import { DocumentData, useAuth, User } from '../../../../middleware/AuthProvider'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../../../../../../firerbaseConfig'
 
 
-const UserDashboard = ({userDataFromAuth:uDA, userDataFromDB:uDB}:ComponentPropsType) => {
-  console.log(uDA)
+const UserDashboard = () => {
+  const UserAuth = useRef<User | undefined>(undefined)
+  const UserDB = useRef<DocumentData | undefined>(undefined)
+  const user = useAuth();
+  useEffect(()=>{
+    const subscription = async ()=>{
+      if(user && user.currentUser){
+        const newDOCRef = doc(db, 'users',user.currentUser.uid)
+        const snapshots = await getDoc(newDOCRef)
+        if(snapshots.exists()){
+          UserAuth.current = user.currentUser
+          UserDB.current = snapshots.data()
+        }
+      }
+    }
+    return ()=>{
+      subscription.unsubscribe()
+    }
+  })
   return(
     <React.Fragment>
-      {uDA?.email}
+      {UserAuth.current && UserAuth.current.email}
       <Typography>
-        <span>Welcome: </span>{uDB?.username}
+        <span>Welcome: </span>{UserDB.current?.username}
       </Typography>
     </React.Fragment>
   )
