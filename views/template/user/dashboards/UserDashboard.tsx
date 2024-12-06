87da0decba705ff82aa337a5cc7500c661be9642
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-12-06 05:36:46 +0100

    add chatwidget

--- views/template/user/dashboards/UserDashboard.tsx
+++ views/template/user/dashboards/UserDashboard.tsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import { Typography } from '@mui/material'
-import { ComponentPropsType } from '../../../../middleware/helpers/ControllerTmplateView'
+import { DocumentData, useAuth, User } from '../../../../middleware/AuthProvider'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../../../../../../firerbaseConfig'
+import ChatWidget from '../../../partials/ChatWidget'
 
 
-const UserDashboard = ({userDataFromAuth:uDA, userDataFromDB:uDB}:ComponentPropsType) => {
-  console.log(uDA)
+const UserDashboard = () => {
+  const UserAuth = useRef<User | undefined>(undefined)
+  const UserDB = useRef<DocumentData | undefined>(undefined)
+  const user = useAuth();
+  useEffect(()=>{
+    (async ()=>{
+      if(user && user.currentUser){
+        const newDOCRef = doc(db, 'users',user.currentUser.uid)
+        const snapshots = await getDoc(newDOCRef)
+        if(snapshots.exists()){
+          UserAuth.current = user.currentUser
+          UserDB.current = snapshots.data()
+        }
+      }
+    })()
+    return ()=>{}
+  })
   return(
     <React.Fragment>
-      {uDA?.email}
-      <Typography>
-        <span>Welcome: </span>{uDB?.username}
-      </Typography>
+      {(UserAuth.current && UserDB.current)? (
+        <React.Fragment>
+          <ChatWidget/>
+          <Typography>
+            <span>Welcome: </span>{UserDB.current?.username}
+          </Typography>
+        </React.Fragment>
+      ):(
+        <React.Fragment>
+          <ChatWidget/>
+          <Typography>Loading....</Typography>
+        </React.Fragment>
+      )}
     </React.Fragment>
   )
 }
