32a713e6312228228acf9099e9a7383df1bde705
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-12-06 05:40:39 +0100

    Update Fallback.tsx

--- views/layouts/Fallback.tsx
+++ views/layouts/Fallback.tsx
@@ -1,1 +1,19 @@
+import { Typography,Stack } from '@mui/material'
+import React from 'react'
+import {IconContext} from 'react-icons'
+import {CgSpinner} from 'react-icons/cg'
 
+const Fallback = () => {
+  return (
+    <React.Fragment>
+        <Stack style={{width:"50%",height:"150px",justifyContent:"center", alignItems:"center"}}>
+            <Typography variant="h6" style={{textAlign: "center"}}>...Loading</Typography>
+            <IconContext.Provider value={{color:"red", size:"5rem"}}>
+                <CgSpinner className="spin"/>
+            </IconContext.Provider>
+        </Stack>   
+    </React.Fragment>
+  )
+}
+
+export default Fallback
