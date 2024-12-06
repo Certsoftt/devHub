6f02676886a15d2c4b0b05ed5b64cb9b4a62abad
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-12-06 05:51:11 +0100

    Update NavRoute.tsx

--- routes/NavRoute.tsx
+++ routes/NavRoute.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import AuthPage from '../views/partials/AuthPage'
-import BounceLoader from 'react-spinner'
 import RequireAuth from '../middleware/RequireAuth'
 import UserDashboard from '../views/template/user/dashboards/UserDashboard'
 // import ForgotEmail from '../views/forms/ForgotEmail'
 // import UpdateProfile from '../views/forms/UpdateProfileForm'
-import ControllerTmplateView from '../middleware/helpers/ControllerTmplateView'
-// import BeatLoader from 'react-spinner'
+import NoAuth from '../middleware/NoAuth'
+import Fallback from '../views/layouts/Fallback'
+import PhoneForm from '../views/forms/PhoneForm'
 
 const LoginForm = React.lazy(()=>import('../views/forms/LoginForm'))
 const SignUpForm = React.lazy(()=>import('../views/forms/SignUpForm'))
@@ -17,14 +17,12 @@ const NavRoute = () => {
     <React.Fragment>
         <Routes>
             <Route path="/" element={<AuthPage/>}>
-                <Route index element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><LoginForm/></React.Suspense>}/>
-                <Route path="/login" element={<React.Suspense fallback={<BounceLoader size={72} color="orange" loading="true"/>}><LoginForm/></React.Suspense>}/>
-                <Route path="/signup" element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><SignUpForm/></React.Suspense>}/>
+                <Route index element={<React.Suspense fallback={<Fallback/>}><LoginForm/></React.Suspense>}/>
+                <Route path="/login" element={<React.Suspense fallback={<Fallback/>}><NoAuth><LoginForm/></NoAuth></React.Suspense>}/>
+                <Route path="/signup" element={<React.Suspense fallback={<Fallback/>}><NoAuth><SignUpForm/></NoAuth></React.Suspense>}/>
             </Route>
-            <Route path="/dashboard/:userId" 
-            element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}>
-              <RequireAuth><ControllerTmplateView component={UserDashboard}/></RequireAuth></React.Suspense>}/>
-            {/* <Route path="/dashboard/update-profile/:userId" element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><RequireAuth><UpdateProfile/></RequireAuth></React.Suspense>}/> */}
+            <Route path="/dashboard/:userId" element={<React.Suspense fallback={<Fallback/>}><RequireAuth><UserDashboard/></RequireAuth></React.Suspense>}/>
+            <Route path="/otp" element={<React.Suspense fallback={<Fallback/>}><NoAuth><PhoneForm/></NoAuth></React.Suspense>}/>
             {/* <Route path="/forgot" element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><ForgotEmail/></React.Suspense>}/> */}
             {/* <Route path="/google-signin" element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><ForgotEmail/></React.Suspense>}/> */}
             {/* <Route path="/phone-signin" element={<React.Suspense fallback={<BounceLoader size={72} color="red" loading="true"/>}><ForgotEmail/></React.Suspense>}/> */}
